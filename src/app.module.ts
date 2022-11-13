c17ee2abe7aba240a33c45c51a278d5b0ae462b1
mkt-engr <mkt.it.engr@gmail.com>
2022-11-13 21:55:52 +0900

    環境変数使えるように設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PuppeteerModule } from './puppeteer/puppeteer.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [PuppeteerModule],
+  imports: [PuppeteerModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
