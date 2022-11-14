8ad362e72291dac8e285ea45db549be0ed6b0345
mkt-engr <mkt.it.engr@gmail.com>
2022-11-14 21:46:59 +0900

    path変更

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -4,11 +4,8 @@ const os = require('os');
  * @type {import("puppeteer").Configuration}
  */
 
-const rootPath = process.env.ENV === 'LOCAL' ? os.homedir() : __dirname;
-// console.log(os.homedir(), 'os.homedir()');
-// console.log(__dirname, '__dirname');
+const rootPath = process.env.ENV === 'LOCAL' ? __dirname : os.homedir();
 module.exports = {
   // Changes the cache location for Puppeteer.
-  // cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
   cacheDirectory: join(rootPath, '.cache', 'puppeteer'),
 };
