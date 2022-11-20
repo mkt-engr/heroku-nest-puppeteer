25d79873718853fba336a8d549607280bd0576bc
Makito Mori <mkt-engr@gmail.com>
2022-11-20 22:19:14 +0900

    puppetter.config.js

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -5,7 +5,11 @@ const os = require('os');
  */
 
 const rootPath = process.env.DYNO ? __dirname : os.homedir();
+console.log({ rootPath });
+console.log('os.homedir()', os.homedir());
+console.log('__dirname', __dirname);
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(rootPath, '.cache', 'puppeteer'),
+  // cacheDirectory: join(rootPath, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
